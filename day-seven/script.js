b0773adad09eba88fd151c637a34a5e9dfd47f71
Evelinn <evelinn.idenfors@gmail.com>
2022-02-23 16:20:52 +0100

    added transition and on hover effect

---
+++ day-seven/script.js
@@ -0,0 +1,12 @@
+// window.onload = () => {
+//   const CLASS_LEFT_IMAGE = "left";
+//   const CLASS_RIGHT_IMAGE = "right";
+
+//   function addWidthLeft() {
+//     document.getElementsByClassName(CLASS_LEFT_IMAGE).style.width = "150%";
+//   }
+
+//   document.getElementsByClassName(CLASS_LEFT_IMAGE).hover(function() {
+//     addWidthLeft();
+//   });
+// }
