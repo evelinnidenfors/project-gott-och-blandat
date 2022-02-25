9ec7432cc5a33d1534d9742c78bf3947e9ca045b
Evelinn <evelinn.idenfors@gmail.com>
2022-02-25 11:45:14 +0100

    change color on focus and on blur

---
+++ day-eight/script.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+  const labels = document.querySelectorAll(".form label");
+
+  labels.forEach((label) => {
+    label.querySelectorAll('span').forEach((span, idx) => {
+      span.style['transitionDelay'] = `${idx * 50}ms`
+    });
+  });
+
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+
+  email.onfocus = function() {
+    document.querySelectorAll("#email ~ label span").forEach((span) => {
+      span.style.color = "#DDBEA9";
+    });
+  }
+
+  password.onfocus = function() {
+    document.querySelectorAll("#password ~ label span").forEach((span) => {
+      span.style.color = "#DDBEA9";
+    });
+  }
+
+  email.onblur = function() {
+    document.querySelectorAll("#email ~ label span").forEach((span) => {
+      span.style.color = "#fff";
+    });
+  }
+
+  password.onblur = function() {
+    document.querySelectorAll("#password ~ label span").forEach((span) => {
+      span.style.color = "#fff";
+    });
+  }
+};
