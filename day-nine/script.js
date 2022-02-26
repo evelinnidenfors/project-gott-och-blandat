34d65f07793864a1fa194f44b304fc0450c60968
Evelinn <evelinn.idenfors@gmail.com>
2022-02-26 15:39:29 +0100

    updated naming of audio const

--- day-nine/script.js
+++ day-nine/script.js
@@ -1,33 +1,33 @@
 window.onload = () => {
   console.log("loaded")
-  const APPLAUSE = document.getElementById("applause");
-  const BOO = document.getElementById("boo");
-  const GASP = document.getElementById("gasp");
-  const TADA = document.getElementById("tada");
-  const VICTORY = document.getElementById("victory");
-  const WRONG = document.getElementById("wrong");
+  const APPLAUSE_AUDIO = document.getElementById("applause");
+  const BOO_AUDIO = document.getElementById("boo");
+  const GASP_AUDIO = document.getElementById("gasp");
+  const TADA_AUDIO = document.getElementById("tada");
+  const VICTORY_AUDIO = document.getElementById("victory");
+  const WRONG_AUDIO = document.getElementById("wrong");
 
   document.getElementById("applause_btn").onclick = () => {
-    APPLAUSE.play();
+    APPLAUSE_AUDIO.play();
   }
 
   document.getElementById("boo_btn").onclick = () => {
-    BOO.play();
+    BOO_AUDIO.play();
   }
 
   document.getElementById("gasp_btn").onclick = () => {
-    GASP.play();
+    GASP_AUDIO.play();
   }
 
   document.getElementById("tada_btn").onclick = () => {
-    TADA.play();
+    TADA_AUDIO.play();
   }
 
   document.getElementById("victory_btn").onclick = () => {
-    VICTORY.play();
+    VICTORY_AUDIO.play();
   }
 
   document.getElementById("wrong_btn").onclick = () => {
-    WRONG.play();
+    WRONG_AUDIO.play();
   }
 }
