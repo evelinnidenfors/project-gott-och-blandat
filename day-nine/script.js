1702be95ad244c884ddfc8ef2945b51d9b9105ef
Evelinn <evelinn.idenfors@gmail.com>
2022-02-26 15:50:41 +0100

    cleaning up code and removing repetitiveness

--- day-nine/script.js
+++ day-nine/script.js
@@ -1,33 +1,13 @@
 window.onload = () => {
-  console.log("loaded")
-  const APPLAUSE_AUDIO = document.getElementById("applause");
-  const BOO_AUDIO = document.getElementById("boo");
-  const GASP_AUDIO = document.getElementById("gasp");
-  const TADA_AUDIO = document.getElementById("tada");
-  const VICTORY_AUDIO = document.getElementById("victory");
-  const WRONG_AUDIO = document.getElementById("wrong");
+  const audioNames = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
 
-  document.getElementById("applause_btn").onclick = () => {
-    APPLAUSE_AUDIO.play();
+  function setupAudio(audioName) {
+    document.getElementById(audioName + "_btn").onclick = () => {
+      document.getElementById(audioName).play();
+    };
   }
 
-  document.getElementById("boo_btn").onclick = () => {
-    BOO_AUDIO.play();
-  }
-
-  document.getElementById("gasp_btn").onclick = () => {
-    GASP_AUDIO.play();
-  }
-
-  document.getElementById("tada_btn").onclick = () => {
-    TADA_AUDIO.play();
-  }
-
-  document.getElementById("victory_btn").onclick = () => {
-    VICTORY_AUDIO.play();
-  }
-
-  document.getElementById("wrong_btn").onclick = () => {
-    WRONG_AUDIO.play();
-  }
-}
+  audioNames.forEach(audioName => {
+    setupAudio(audioName);
+  });
+};
