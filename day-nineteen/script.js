7333c898cc83b670a763a7b579c704627e48b00a
Evelinn <evelinn.idenfors@gmail.com>
2022-02-28 22:12:10 +0100

    adding movement to pointers for min hour and sec

--- day-nineteen/script.js
+++ day-nineteen/script.js
@@ -4,6 +4,9 @@ window.onload = () => {
   const DARK_MODE = "dark";
   const DATE_OUTPUT = document.querySelector(".date");
   const TIME_OUTPUT = document.querySelector(".time");
+  const SECONDS_NEEDLE = document.querySelector(".second");
+  const MINUTES_NEEDLE = document.querySelector(".minute");
+  const HOURS_NEEDLE = document.querySelector(".hour");
 
   // Setting dark or light mode on click of button
   TOGGLE.addEventListener("click", (e) => {
@@ -40,16 +43,29 @@ window.onload = () => {
     "Dec",
   ];
 
-  const d = new Date();
-  const dayOfWeek = days[d.getDay()];
-  const month = months[d.getMonth()];
-  const date = d.getDate();
-  const hour = d.getHours();
-  const hoursDisplay = hour % 12;
-  const minutes = d.getMinutes();
-  const seconds = d.getSeconds();
-  const am_pm = hour >= 12 ? 'PM' : 'AM';
 
+  function timeMovement() {
+    const d = new Date();
+    const dayOfWeek = days[d.getDay()];
+    const month = months[d.getMonth()];
+    const date = d.getDate();
+    const hour = d.getHours();
+    const hoursDisplay = hour % 12;
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
+    const am_pm = hour >= 12 ? "PM" : "AM";
+
+    SECONDS_NEEDLE.style.transform = `translate(-50%, -100%) rotate(${360/60 * seconds}deg)`;
+    MINUTES_NEEDLE.style.transform = `translate(-50%, -100%) rotate(${360/60 * minutes}deg)`;
+    HOURS_NEEDLE.style.transform = `translate(-50%, -100%) rotate(${360/12 * hour}deg)`;
+
+    TIME_OUTPUT.innerHTML = `${hoursDisplay}:${
+      minutes < 10 ? `0${minutes}` : minutes
+    } ${am_pm}`;
     DATE_OUTPUT.innerHTML = `${dayOfWeek}, ${month} <span class="circle">${date}</span>`;
-    TIME_OUTPUT.innerHTML = `${hoursDisplay}:${minutes < 10 ? `0${minutes}` : minutes} ${am_pm}`
+  };
+  timeMovement()
+
+  setInterval(timeMovement, 1000);
+
 };
