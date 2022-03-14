d53c33a9eec8229bb17287d57685453f75e67ff5
Evelinn <evelinn.idenfors@gmail.com>
2022-03-14 19:04:13 +0100

    logic for all glasses to the left or right updating too

--- day-sixteen/script.js
+++ day-sixteen/script.js
@@ -1,20 +1,41 @@
 window.onload = () => {
   const GLASSES = document.querySelectorAll(".small-glass");
+  const FULL_CLASS = "full";
 
   updateBigGlass();
 
   GLASSES.forEach((glass) => {
-    const FULL_CLASS = "full";
 
-    glass.addEventListener("click", function fillGlass() {
+    glass.addEventListener("click", function fillGlass(event) {
+
       if (glass.classList.contains(FULL_CLASS)) {
+        updateNextGlass(event.target.nextElementSibling);
         glass.classList.remove(FULL_CLASS);
       } else {
+        updatePreviousGlass(event.target.previousElementSibling);
         glass.classList.add(FULL_CLASS);
       }
       updateBigGlass();
     });
   });
+
+  function updatePreviousGlass(element) {
+    if (element == null) {
+      return;
+    } else {
+      element.classList.add(FULL_CLASS);
+      updatePreviousGlass(element.previousElementSibling);
+    }
+  }
+
+  function updateNextGlass(element) {
+    if (element == null) {
+      return;
+    } else {
+      element.classList.remove(FULL_CLASS);
+      updateNextGlass(element.nextElementSibling);
+    }
+  }
 
   function updateBigGlass() {
     const PERCENTAGE = document.getElementById("percentage");
@@ -27,11 +48,13 @@ window.onload = () => {
     if (NUMBER_OF_FULL_GLASSES === 0) {
       PERCENTAGE.style.height = 0;
       PERCENTAGE.style.display = "none";
+      LITERS_LEFT.innerHTML = 2 + "L";
     } else {
       PERCENTAGE.innerHTML = PERCENTAGE.style.height =
         (NUMBER_OF_FULL_GLASSES / NUMBER_OF_GLASSES) * 100 + "%";
       PERCENTAGE.style.display = "flex";
-      LITERS_LEFT.innerHTML = GLASS_MAX - (GLASS_VOLUME * NUMBER_OF_FULL_GLASSES) + "L";
+      LITERS_LEFT.innerHTML =
+        GLASS_MAX - GLASS_VOLUME * NUMBER_OF_FULL_GLASSES + "L";
     }
   }
 };
