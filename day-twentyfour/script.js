30feea657f873183c75e3f77a328711dc7b839b9
Evelinn <evelinn.idenfors@gmail.com>
2022-03-20 13:53:27 +0100

    adding author

--- day-twentyfour/script.js
+++ day-twentyfour/script.js
@@ -6,9 +6,19 @@ window.onload = () => {
   const HEADER = document.getElementById("header");
   const PARAGRAPH = document.getElementById("paragraph");
 
+  const AUTHOR_IMAGE_CONTAINER = document.querySelector('.author-img');
+  const AUTHOR_IMG = document.createElement('img');
+  AUTHOR_IMG.src = 'https://randomuser.me/api/portraits/women/18.jpg'
+  const NAME = document.getElementById('name');
+  const DATE = document.getElementById('date');
+
   IMAGE_CONTAINER.appendChild(image_elem);
 
   HEADER.innerHTML = "Lorem Ipsum";
   PARAGRAPH.innerHTML =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  AUTHOR_IMAGE_CONTAINER.appendChild(AUTHOR_IMG);
+  NAME.innerHTML = 'Jane Doe';
+  DATE.innerHTML = 'March 20, 2022';
 };
