44ccb0cecd97a64612f613d38fa93db74eac289a
Evelinn <evelinn.idenfors@gmail.com>
2022-03-20 12:36:44 +0100

    adding hover class on hover of new box

--- day-twentyone/script.js
+++ day-twentyone/script.js
@@ -1,3 +1,42 @@
 window.onload = () => {
+  const IMAGE = document.getElementById("image");
+  const BOXES = document.querySelectorAll(".box");
+  const HOVER_CLASS = ' hovering';
 
+  IMAGE.addEventListener('dragstart', dragStart);
+  IMAGE.addEventListener('dragend', dragEnd);
+
+  function dragStart(e) {
+
+  };
+
+  function dragEnd(e) {
+
+  };
+
+
+  BOXES.forEach(box => {
+    box.addEventListener('dragenter', dragEnter);
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+  });
+
+  function dragEnter(e) {
+    e.preventDefault();
+    this.className += HOVER_CLASS;
+  };
+
+  function dragOver(e) {
+    e.preventDefault();
+  };
+
+  function dragLeave(e) {
+    e.preventDefault();
+    this.className = 'box';
+  };
+
+  function drop(e) {
+    console.log(e)
+  };
 }
