25218408aa6def00e515fe7813e83a14576c543f
Evelinn <evelinn.idenfors@gmail.com>
2022-03-29 22:11:58 +0200

    final adjustments for transform to loop

--- day-thirtyfive/script.js
+++ day-thirtyfive/script.js
@@ -2,7 +2,7 @@ window.onload = () => {
   const PREV_BTN = document.getElementById("prev-btn");
   const NEXT_BTN = document.getElementById("next-btn");
   const IMAGE_CONTAINER = document.getElementById("image-container");
-  const IMAGES = document.querySelectorAll(".image-container > img");
+  const IMAGES = document.querySelectorAll("#image-container > img");
   const IMAGE_LENGTH = IMAGES.length;
 
   let translateIndex = 0;
@@ -13,9 +13,15 @@ window.onload = () => {
 
   function spinCarousel(direction) {
     if (direction === 'prev') {
-      translateIndex -= 500;
-    } else if (direction === 'next') {
       translateIndex += 500;
+      if (translateIndex > 0) {
+        translateIndex = 0;
+      }
+    } else if (direction === 'next') {
+      translateIndex -= 500;
+      if (translateIndex < -(500 * IMAGE_LENGTH - 1)) {
+        translateIndex = 0;
+      }
     }
 
     IMAGE_CONTAINER.style.transform = `translateX(${translateIndex}px)`;
